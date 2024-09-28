df278772ca50d08e1ad138293a141e44be5bde76
MarikPG <120489191+MarikPG@users.noreply.github.com>
2024-09-28 17:29:36 +0300

    smart_notes_update

--- notes/main.py
+++ notes/main.py
@@ -10,16 +10,33 @@ class Widget(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.btn_make.clicked.connect(self.add_note)
-        self.show_note()
-
-    def show_note(self):
-        self.ui.list_1.clear()
-        self.ui.list_2.clear()
-        print(notes.keys())
+        self.ui.list_1.itemClicked.connect(self.show_note)
         for note in notes.keys():
             self.ui.list_1.addItem(note)
-            self.ui.list_2.addItems(notes[note]["tags"])
+        self.ui.btn_save.clicked.connect(self.save_note)
+        self.ui.btn_add.clicked.connect(self.add_tag)
 
+    
+    def show_note(self, item):
+        self.ui.list_2.clear()
+        note_name= item.text()
+        if note_name in notes:
+            self.ui.list_2.addItems(notes[note_name]["теги"])
+            self.ui.textEdit_2.setText(notes[note_name]["текст"])
+
+    def save_note(self):
+        if self.ui.list_1.currentItem():
+            key = self.ui.list_1.currentItem().text()
+            notes[key]["текст"] = self.ui.textEdit_2.toPlainText()
+        self.write_to_file()
+
+    def add_tag(self):
+        if self.ui.list_1.currentItem():
+            tag_name,ok =QInputDialog.getText(self, "Додати тег", "Введіть тег")
+            if tag_name and ok:
+                key = self.ui.list_1.currentItem().text()
+                notes[key]["теги"].append(tag_name)
+        self.write_to_file()
 
     def add_note(self):
         note_name,ok = QInputDialog.getText(self, "Додати замітку", "Назва замітки")
@@ -27,8 +44,13 @@ class Widget(QMainWindow):
         if ok and note_name != "":
             notes[note_name] = {"теги": [], "текст": ""}
             self.ui.list_1.addItem(note_name)
+        self.write_to_file()
+
+    def write_to_file(self):
+        with open("notes/notes.json","w", encoding="utf-8") as file:
+            json.dump(notes, file,ensure_ascii=False) 
         
-with open("notes/notes.json","r", encoding ="utf 8") as file:
+with open("notes/notes.json","r", encoding ="utf-8") as file:
     notes = json.load(file)
 print(notes)
 
--- steet/streets.py
+++ steet/streets.py
@@ -1,21 +1,22 @@
-filename = "file.txt"
+streets = "file.txt"
 
-def print_file_content(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
+def print_streets(streets):
+    with open(streets, 'r', encoding='utf-8') as file:
         content = file.readlines()
         if content:
             for line in content:
                 print(line)
+print_streets(streets)
 
-def add_street(filename):
+def add_street(streets):
     new_entry = input("Введіть новий запис: ")
-    with open(filename, 'a', encoding='utf-8') as file:
+    with open(streets, 'a', encoding='utf-8') as file:
         file.write(new_entry + '\n')
     print("Вулицю додано.")
 
-def search_in_file(filename):
+def search_street(streets):
     keyword = input("Введіть ключове слово для пошуку: ").lower() 
-    with open(filename, 'r', encoding='utf-8') as file:  
+    with open(streets, 'r', encoding='utf-8') as file:  
         found = False  
         for line in file:
             if keyword in line.lower(): 
@@ -30,10 +31,10 @@ while True:
             print("Програма завершила роботу.")
             break
         elif command == 'додати':
-            add_street(filename)
+            add_street(streets)
         elif command == 'друкувати':
-            print_file_content(filename)
+            print_streets(streets)
         elif command == 'пошук':
-            search_in_file(filename)
+            search_street(streets)
         else:
             print("Невідома команда. Спробуйте ще раз.")
